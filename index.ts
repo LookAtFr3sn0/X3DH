77c62ad1a8ec953fe2d8ab105b37ab7cc49a264e
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-05-18 03:43:10 +0200

    Add sodium-native dependency and refactor code

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
 import { X3DH } from "./lib/sodium.js";
+
+const x3dh = new X3DH('x25519', 'sha256', 'MyProtocol');
+console.log(x3dh);
--- lib/sodium.ts
+++ lib/sodium.ts
@@ -1,10 +1,30 @@
+import sodium from 'sodium-native';
+
 export class X3DH {
   private curve: 'x25519' | 'x448';
   private hash: 'sha256' | 'sha512';
   private info: string;
-  constructor (curve: 'x25519' | 'x448' = 'x25519', hash: 'sha256' | 'sha512' = 'sha512', info: string = 'MyProtocol') {
+  private sodium: typeof sodium;
+
+  constructor (curve: 'x25519' | 'x448' = 'x25519', hash: 'sha512' | 'sha512' = 'sha512', info: string = 'MyProtocol') {
     this.curve = curve;
     this.hash = hash;
     this.info = info;
+    this.sodium = sodium;
   }
+
+  public getCurve(): 'x25519' | 'x448' {
+    return this.curve;
+  }
+  public getHash(): 'sha256' | 'sha512' {
+    return this.hash;
+  }
+  public getInfo(): string {
+    return this.info;
+  }
+  public getSodium(): typeof sodium {
+    return this.sodium;
+  }
+
+
 }
